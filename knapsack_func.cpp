a84427effef2d1f8b4fc4408dd19211edc30d844
xzhangfordham <42500181+xzhangfordham@users.noreply.github.com>
2024-11-05 17:18:49 -0500

    Update knapsack_func.cpp

--- knapsack_func.cpp
+++ knapsack_func.cpp
@@ -26,6 +26,17 @@ int Knapsack_recursive (const vector<Item> & items, int weight_capacity, int n)
    
 }
 
+//Return maximum value achievable by choosing objects from the first n element in "items" vector
+// and also set the chosen vector to be the index of items chosen (e.g., [1,2] if first and second
+//items are chosen) that achieves the max. value.
+//subject to the contraint that the total weight of chosen objects is less than weight_capacity
+//Each item can be chosen at most once 
+int Knapsack_recursive_full (const vector<Item> & items, int weight_capacity, int n, vector<int> & chosen)
+{
+
+
+}
+
 //DP with Tabulation
 //Return maximum value achievable by choosing objects from the "items" vector 
 // subject to the contraint that the total weight of chosen objects is less than weight_capacity
