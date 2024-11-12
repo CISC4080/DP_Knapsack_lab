bebadc27a849c927b416c73cc433e0e1753001c8
xzhangfordham <42500181+xzhangfordham@users.noreply.github.com>
2024-11-11 21:56:34 -0500

    Update knapsack_func.h

--- knapsack_func.h
+++ knapsack_func.h
@@ -21,16 +21,26 @@ int Knapsack_recursive_full (const vector<Item> & items, int weight_capacity, in
 // subject to the contraint that the total weight of chosen objects is less than weight_capacity
 // and set the "chosen" vector to include the indices of items chosen 
 //Each item can be chosen at most once 
-int Knapsack_tabulation (const vector<Item> & items, int weight_capacity, vector<int> & chosen);
+int Knapsack_tabulation (const vector<Item> & items, int weight_capacity);
 
-
+//Extra credits: DP with Tabulation
+//Return maximum value achievable by choosing objects from the "items" vector 
+// subject to the contraint that the total weight of chosen objects is less than weight_capacity
+// and set the "chosen" vector to include the indices of items chosen 
+//Each item can be chosen at most once 
+int Knapsack_tabulation_full (const vector<Item> & items, int weight_capacity, vector<int> & chosen);
 
 //////////////////////////Unlimited Knapsack /////////////////////////////
 
 //Return maximum value achievable by choosing objects from "items" vector 
 //subject to the contraint that the total weight of chosen objects is less than weight_capacity
 //Each item can be chosen multiple times (i.e., repetition allowed) 
-int Unlimited_Knapsack_recursive (const vector<Item> & items, int weight_capacity, vector<int> & chosen);
+int Unlimited_Knapsack_recursive (const vector<Item> & items, int weight_capacity);
+
+//Extra Credit: Return maximum value achievable by choosing objects from "items" vector 
+//subject to the contraint that the total weight of chosen objects is less than weight_capacity
+//Each item can be chosen multiple times (i.e., repetition allowed) 
+int Unlimited_Knapsack_recursive_full (const vector<Item> & items, int weight_capacity, vector<int> & chosen);
 
 //Return maximum value achievable by choosing objects from "items" vector 
 //subject to the contraint that the total weight of chosen objects is less than weight_capacity
